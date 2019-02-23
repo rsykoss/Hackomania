513257f562d21c9bac0cb63d9fee72aabbd99430
Jason-CKY <jasoncky96@gmail.com>
2019-02-23 16:38:32 +0800

    s

--- Website/sketch.js
+++ Website/sketch.js
@@ -1,27 +1,6 @@
-class Pixie{
-	constructor(x, y, r){
-		this.x = x;
-		this.y = y;
-		this.r = r;
-	}
-
-	draw(){
-		fill(255, 0, 0);
-		ellipse(this.x, this.y, this.r);
-	}
-}
-
-
-let pixie;
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	pixie = new Pixie(50, 50, 10);
 }
 
 function draw() {
-	background(0);
-	pixie.draw();
-	pixie.x = mouseX;
-	pixie.y = mouseY;
 }
